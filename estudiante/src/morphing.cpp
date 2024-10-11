b3a0da03a8e2186af8e39b98cf0d28fc2d072792
Syn . <synnekerinho@gmail.com>
2024-10-11 13:17:14 +0200

    Implemented basic morhping function

--- estudiante/src/morphing.cpp
+++ estudiante/src/morphing.cpp
@@ -4,33 +4,33 @@
 #include <cassert>
 using namespace std;
 Video Morphing(const Image &I1,const Image &I2,int nframes){
-Video aux(nframes);
-if ( I1.get_cols() != I2.get_cols() ||I1.get_rows() != I2.get_rows())
-{
-    cout << "Error: Las imágenes no tienen dimensiones iguales\n";
-    exit(1);
-}
-// iterar sobre cada frame
- for (int i = 0; i < nframes; i++)
- {
-    // Calcular t según posición en el array del video
-    float t = (i / (float)nframes);
-    // Iterar sobre cada pixel del frame i
-    for (int j = 0; j < I1.get_rows(); j++)
+    Video aux(nframes);
+    // Comprobar dimensiones de las imágenes
+    if ( I1.get_cols() != I2.get_cols() ||I1.get_rows() != I2.get_rows())
     {
-        for (int k = 0; k < I1.get_cols(); k++)
+        cout << "Error: Las imágenes no tienen dimensiones iguales\n";
+        exit(1);
+    }
+    // iterar sobre cada frame
+    for (int i = 0; i < nframes; i++)
+    {
+        // Calcular t según posición en el array del video
+        float t = (i / (float)nframes);
+        // Iterar sobre cada pixel del frame i
+        for (int j = 0; j < I1.get_rows(); j++)
         {
-            // Calcular indice para get_pixel
-            int index = k + j*I1.get_cols();
-            // Calcular fórmula morphing para pixel (j, k)
-            pixel morphed_pixel = I1.get_pixel(index) * (1-t) + 
-                                  I2.get_pixel(index) * (t);
-            // Asignar valor al pixel actual
-            aux[i].set_pixel(j, k, morphed_pixel);
+            for (int k = 0; k < I1.get_cols(); k++)
+            {
+                // Calcular indice para get_pixel
+                int index = k + j*I1.get_cols();
+                // Calcular fórmula morphing para pixel (j, k)
+                pixel morphed_pixel = I1.get_pixel(index) * (1-t) + 
+                                    I2.get_pixel(index) * (t);
+                // Asignar valor al pixel actual
+                aux[i].set_pixel(j, k, morphed_pixel);
+            }
         }
     }
- }
-
 }
 
 int main(int argc, char * argv[]){
