b4f33d2d6e45e3140382862d45aca5739eb1ead2
Syn . <synnekerinho@gmail.com>
2024-10-11 13:14:41 +0200

    Implemented basic morhping function

--- estudiante/src/morphing.cpp
+++ estudiante/src/morphing.cpp
@@ -10,16 +10,22 @@ if ( I1.get_cols() != I2.get_cols() ||I1.get_rows() != I2.get_rows())
     cout << "Error: Las imágenes no tienen dimensiones iguales\n";
     exit(1);
 }
+// iterar sobre cada frame
  for (int i = 0; i < nframes; i++)
  {
+    // Calcular t según posición en el array del video
     float t = (i / (float)nframes);
+    // Iterar sobre cada pixel del frame i
     for (int j = 0; j < I1.get_rows(); j++)
     {
         for (int k = 0; k < I1.get_cols(); k++)
         {
+            // Calcular indice para get_pixel
             int index = k + j*I1.get_cols();
+            // Calcular fórmula morphing para pixel (j, k)
             pixel morphed_pixel = I1.get_pixel(index) * (1-t) + 
                                   I2.get_pixel(index) * (t);
+            // Asignar valor al pixel actual
             aux[i].set_pixel(j, k, morphed_pixel);
         }
     }
